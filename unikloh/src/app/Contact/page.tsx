840646aae31a96535b624ec26cda644a17fbc1cb
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-11-13 02:10:00 +0800

    feat: dynamic product page

---
+++ unikloh/src/app/Contact/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+
+        Call Us At: +62 812 1111 2222
+      
+    </div>
+  )
+}
+
+export default Contact
--- unikloh/src/app/component/header/Header.tsx
+++ unikloh/src/app/component/header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div  className='sticky top-0 z-50 shadow h-[10vh] bg-red-600 flex items-center justify-center text-white font-bold'>
-        <span className='mr-4'>Home</span>
+        <span className='mr-4'><Link href="/">Home</Link></span>
         <span className='mr-4'>Product</span>
         <span className='mr-4'>Logo</span> 
-        <span className='mr-4'>Contact Us</span> 
+        <span className='mr-4'><Link href='/Contact/'>Contact Us</Link></span> 
         <span className='mr-4'>Profile</span>
         <span className='mr-4'>Login</span>
     </div>
