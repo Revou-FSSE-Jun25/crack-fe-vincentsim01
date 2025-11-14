d0d1d27d30d5fa2c89e2f88603c4bb2bda506af0
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-11-14 15:55:31 +0800

    feat: login and signup page

---
+++ unikloh/src/lib/api/api.ts
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://dummyjson.com';
+
+export const api = {
+//       getProducts: async (limit: number = 10) => {
+//     const response = await fetch(`${BASE_URL}/products?limit=${limit}`);
+
+//     if (!response.ok) {
+//       throw new Error('Failed to fetch products');
+//     }
+
+//     const data = await response.json();
+//     return data.products;
+//   }
+}
