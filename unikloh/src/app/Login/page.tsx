8091fa06e1d2e12ac78428cc58b357619960ff66
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-11-14 16:38:43 +0800

    feat: login page template

--- unikloh/src/app/Login/page.tsx
+++ unikloh/src/app/Login/page.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+"use client"
+
+import React,{useState, useEffect} from 'react';
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+    const router = useRouter();
+    const initialValue = {
+        username: 'Arthur',
+        password: 'arthur123'
+    }
+
+    const [formData, setFormData] = useState(initialValue);
+
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>){
+        const {name, value} = event.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    function handleLogin(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        console.log("Form submitted:", formData);
+        router.push("/");
+    }
   return (
     <div>
 
-        Login Page
+        <form onSubmit={handleLogin}>
+            <label htmlFor='username'>Username</label>
+            <input type='text' id='username' name='username' value={formData.username} onChange={handleChange} placeholder='username' required />
+            <br/>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' name='password' value={formData.password} onChange={handleChange} placeholder='password' required />
+            <br/>
+            <button type='submit'>Login</button>
+        </form>
       
     </div>
   )
