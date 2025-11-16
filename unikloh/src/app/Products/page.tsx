4613a123999a0132a4e224e1a739f2f20f5e3600
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-11-17 02:04:53 +0800

    feat: product fetch on product page

--- unikloh/src/app/Products/page.tsx
+++ unikloh/src/app/Products/page.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { api } from '@/lib/api/api';
 
-const page = () => {
+
+const page = async () => {
+
+  const fetchedData = await api.getProducts(10);
+
+
+  
   return (
     <div>
 
         This is the Products page.
+      <div className='flex flex-wrap '>
+        {fetchedData.map((item:any)=>{
+          return (
+            <div className='m-5 p-5 border-2 border-black w-[30%]' key={item.id}>
+
+              <h2 key={item.id}>{item.title}</h2>              
+              <div key={item.id}>{item.title}</div>
+              <div key={item.id}>{item.description}</div>
+              <div key={item.id}>${item.price}</div>
+              <img key={item.id} src={item.thumbnail} alt={item.title} width={200} height={200}/>
+              <br></br>
+              <button className='mt-2 p-2 bg-blue-500 text-white rounded'>Add to Cart</button>
+            </div>
+          )
+
+
+
+        })}
+
+        </div>
       
     </div>
   )
