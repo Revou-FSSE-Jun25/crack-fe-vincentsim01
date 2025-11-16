80655a6825eb77ce1ae9e3d66d4a2494efc7ce6a
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-11-17 03:38:40 +0800

    feat: blog page create skeleton

--- unikloh/src/app/component/addtocartbutton/AddToCartButton.tsx
+++ unikloh/src/app/component/addtocartbutton/AddToCartButton.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, {useState,useEffect} from 'react';
-import {Link} from 'next/link';
+// import {Link} from 'next/link';
 
 const AddToCartButton = ({ title }: { title: string }) => {
     const [isAdding, setIsAdding] = useState(false);
