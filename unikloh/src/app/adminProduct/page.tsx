a4ca4b03271b912b0ae061940376873816105f59
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-11-25 07:18:49 +0800

    feat:create add product form

--- unikloh/src/app/adminProduct/page.tsx
+++ unikloh/src/app/adminProduct/page.tsx
@@ -1,12 +1,114 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import Link from "next/link";
+import {Product, ProductFormData} from '@/types/product';
 
 const AdminProduct = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=10", {
+      method: "GET",
+      headers: {}})
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+    let initialAddProduct: ProductFormData = {
+    title: "Shirt A",
+    price: 100,
+    description: "A shirt that is made for A",
+    categoryId: 1,
+   
+  }
+
+  const [formData, setFormData] = useState<ProductFormData>(initialAddProduct);
+
+
+  function handleAddProduct() {
+    // Logic to add a new product
+    fetch("https://api.escuelajs.co/api/v1/products", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+          },
+      body: JSON.stringify({
+      // Add product data here
+          title: formData.title,
+          price: formData.price,
+          description: formData.description,
+          categoryId: formData.categoryId,
+          images: ["https://placehold.co/600x400"]
+      })
+  }) .then(res => res.json())
+  .then(data => {
+    console.log("Product created:", data);
+  })
+}
+
+function openAddProductModal() {
+  const modal = document.getElementById('addProductModal');
+  
+    modal?.classList.toggle('hidden');
+  
+}
   return (
     <div>
-        <button>Add New Product</button>
-        Product 1
-        Price $50
-        <button>Edit</button>
+      Product Admin
+        <button className='border p-2 rounded bg-blue-500 text-white' onClick={openAddProductModal}>Add New Product</button>
+
+          <div className='hidden absolute top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center' id='addProductModal'>
+          <h2>Add New Product</h2>
+            <form onSubmit={(e) => { e.preventDefault(); handleAddProduct(); }}>
+              <label htmlFor='title'>Title:</label>
+              <input
+                type='text'
+                id='title'
+                value={formData.title}
+                onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+              />
+              <br />
+              <label htmlFor='price'>Price:</label>
+              <input
+                type='number'
+                id='price'
+                value={formData.price}
+                onChange={(e) => setFormData({ ...formData, price: Number(e.target.value) })}
+              />
+
+              <br />
+              <label htmlFor='description'>Description:</label>
+              <textarea
+                id='description'
+                value={formData.description}
+                onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+              />
+              <br />
+              <label htmlFor='categoryId'>Category ID:</label>
+              <input
+                type='number'
+                id='categoryId'
+                value={formData.categoryId}
+                onChange={(e) => setFormData({ ...formData, categoryId: Number(e.target.value) })}
+              />
+              <br />
+
+              <button type="submit">Submit</button>
+            </form>
+        </div>
+
+
+        {products.map(product => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <img src={product.images[0]} alt={product.title} />
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+            <button>Edit</button>
+          </div>
+        ))}
+
+      
       
     </div>
   )
