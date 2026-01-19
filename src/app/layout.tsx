e7b2bc9ad5a0a6f97162a3e2e665893b02a16594
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2026-01-20 06:06:18 +0800

    seo meta add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,10 +49,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <title>Unik Loh!</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Welcome to Vincent Kiathadi's portfolio website. Let's connect and explore my work." />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Website Developer In Town | Vincent Kiathadi" />
+        <meta property="og:description" content="Discover award-winning website developer in town. Let's connect and explore all possibilities" />
+        <meta property="og:image" content="https://i.ibb.co/k2pYPYTf/Screenshot-2025-07-03-102851.png" />
+        <meta property="og:type" content="website" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="../assets/icons8-star-48.png" type="image/x-icon" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Unik Loh!</title>
       </head>
       {/* ${geistSans.variable} ${geistMono.variable} */}
       <body
